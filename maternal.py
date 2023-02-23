e34690f18b6e67d01eb42e715863a93abe994279
Ashok D'sa <109778122+Ashokdsa@users.noreply.github.com>
2023-02-24 01:03:04 +0530

    Create maternal.py

---
+++ maternal.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+df = pd.read_csv('https://raw.githubusercontent.com/Ashokdsa/Maternal-health-risk/main/Maternal%20Health%20Risk%20Data%20Set.csv', na_values='?')
+df = df.rename(columns={'RiskLevel': 'target'})
+df['target'].value_counts(dropna=False)
+y = df['target']
+X = df.drop('target', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+#Linear Regression
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+y_lr_train_pred = lr.predict(X_train)
+y_lr_test_pred = lr.predict(X_test)
+from sklearn.metrics import mean_squared_error, r2_score
+lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+lr_results = pd.DataFrame(['Linear regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+#Random Forest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(max_depth=2, random_state=100)
+rf.fit(X_train, y_train)
+y_rf_train_pred = rf.predict(X_train)
+y_rf_test_pred = rf.predict(X_test)
+rf_train_mse = mean_squared_error(y_train, y_rf_train_pred)
+rf_train_r2 = r2_score(y_train, y_rf_train_pred)
+rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
+rf_test_r2 = r2_score(y_test, y_rf_test_pred)
+rf_results = pd.DataFrame(['Random forest', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
+rf_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+#Decision Tree
+X = df.drop('BS', axis=1)
+dt = DecisionTreeRegressor(random_state=0)
+dt = dt.fit(X_train, y_train)
+y_dt_train_pred = dt.predict(X_train)
+y_dt_test_pred = dt.predict(X_test)
+dt_train_mse = mean_squared_error(y_train, y_dt_train_pred)
+dt_train_r2 = r2_score(y_train, y_dt_train_pred)
+dt_test_mse = mean_squared_error(y_test, y_dt_test_pred)
+dt_test_r2 = r2_score(y_test, y_dt_test_pred)
+dt_results = pd.DataFrame(['Decision Tree', dt_train_mse, dt_train_r2, dt_test_mse, dt_test_r2]).transpose()
+dt_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
+#Comparing the Models
+df_models = pd.concat([lr_results, rf_results,dt_results], axis=0)
+df_models.reset_index(drop=True)
+#Getting the inputs
+input_data = (55,110,85,6.9,98,88)
+input_data_as_numpy_array = np.asarray(input_data)
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+prediction=dt.predict(input_data_reshaped)
+if (prediction[0] == 0):
+
+  print("The pregnant patient is at low risk")
+
+elif (prediction[1]==1):
+
+  print('The pregnant patient is at high risk')
+
+else:
+
+  print('The pregnant patient is at medium risk')
